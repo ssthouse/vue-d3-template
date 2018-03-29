6b76452ce10f892a2cf97452ba97ad9534a34dd0
shen_s <ssthouse@163.com>
2018-03-29 17:51:07 +0800

    Clear default component.

---
+++ src/components/Root.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <svg id="svg"></svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Root',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
